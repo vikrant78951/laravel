c6f3b8a2277644fa200888fc4438d899460efa30
Vikas Singh <Vikass@xcellhost.cloud>
2022-01-03 14:56:27 +0530

    initial commit

---
+++ readme.php
@@ -0,0 +1,4 @@
+Laravel is php framework used for build rubost applicaitons
+composer : package manager for Laravel ( Install composer.exe from composer )
+CREATE NEW PROJECT : composer create-project laravel/laravel project-name
+
